262ec80dfbc65775e21d3bac92874cf7fc260958
Pratush <pratushbarua26@gmail.com>
2025-11-14 09:22:47 +0600

    fixed db connection

--- index.js
+++ index.js
@@ -9,14 +9,10 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-app.use(async (req, res, next) => {
-  console.log(
-    `⚡ ${req.method} - ${req.path} from ${
-      req.host
-    } at ⌛ ${new Date().toLocaleString()}`
-  );
-  next();
-});
+// app.use(async (req, res, next) => {
+//   console.log(`⚡ ${req.method} - ${req.path} from ${req.host} at ⌛ ${new Date().toLocaleString()}`);
+//   next();
+// });
 
 //ports & clients
 const port = process.env.PORT || 5000;
@@ -30,30 +26,23 @@ const client = new MongoClient(uri, {
 });
 
 //listeners
-client
-  .connect()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Hero Apps Server listening ${port}`);
-      console.log(`Hero Apps Server Connected with DB`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+client.connect()
+  .then(() => app.listen(port, () => console.log(`Hero Apps Server listening ${port}\nHero Apps Server Connected with DB`)))
+  .catch((err) => console.log(err));
 
 //DB & collections
-const database = client.db("heroAppsDB");
+const database = client.db("heroApps");
 const appsCollection = database.collection("apps");
 
 //Apps Route
 
 app.get("/apps", async (req, res) => {
+  const{limit = 0, skip = 0, sort, search = "" } = req.query
   try {
-    const apps = await appsCollection.find().toArray();
+    const apps = await appsCollection.find({title: { $regex: search, $options: "i" }}).limit(limit).skip(skip).toArray();
     res.send(apps);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
@@ -61,28 +50,16 @@ app.get("/apps", async (req, res) => {
 app.get("/apps/:id", async (req, res) => {
   try {
     const appId = req.params.id;
-
-    if (appId.length != 24) {
-      res.status(400).json({ error: "Invalid ID" });
-      return;
-    }
-    const query = new ObjectId(appId);
-    const app = await appsCollection.findOne({ _id: query });
+    if (appId.length != 24) return res.status(400).json({ error: "Invalid ID" });
+    const app = await appsCollection.findOne({ _id: new ObjectId(appId) });
     res.json(app);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Basic routes
-app.get("/", (req, res) => {
-  res.json({ status: "ok", message: "Hero Apps Server" });
-});
+app.get("/", (req, res) => res.json({ status: "ok", message: "Hero Apps Server" }));
 //404
-app.all(/.*/, (req, res) => {
-  res.status(404).json({
-    status: 404,
-    error: "API not found",
-  });
-});
+app.all(/.*/, (req, res) => res.status(404).json({ status: 404, error: "API not found", }));
